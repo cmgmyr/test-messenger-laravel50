3327e13f0957a9b7207daa549eeac11de02de633
Chris Gmyr <cmgmyr@gmail.com>
2016-08-29 16:16:09 -0400

    upgrade to 2.12

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,36 +1,17 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder {
-
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
-
-		App\User::create([
-            'name' => 'Chris Gmyr',
-            'email' => 'chris@example.com',
-            'password' => Hash::make('pass123'),
-        ]);
-
-        App\User::create([
-            'name' => 'Taylor Otwell',
-            'email' => 'taylor@example.com',
-            'password' => Hash::make('pass123'),
-        ]);
-
-        App\User::create([
-            'name' => 'Adam Wathan',
-            'email' => 'adam@example.com',
-            'password' => Hash::make('pass123'),
-        ]);
-	}
-
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(MessengerSeeder::class);
+    }
 }
